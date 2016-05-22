d79b6d53dbffe41130d5f1d51f64633839a239aa
justinlboyer <justinlboyer@gmail.com>
2016-05-21 21:36:31 -0600

    Modified code so that it would agree with the fl_df

--- dataAnalysis.R
+++ dataAnalysis.R
@@ -5,50 +5,50 @@ source(file="loadData.R" )
 
 #Look at boxplots of avalanche weather and non avalanche weather
 # Precipitation is a big deal
-boxplot(no_a_df$Precipitation, aw_df$Precipitation)
-summary(aw_df$Precipitation)
-summary(no_a_df$Precipitation)
+boxplot(fl_df$Precipitation[is.na(fl_df$Place)], fl_df$Precipitation[!is.na(fl_df$Place)])
+summary(fl_df$Precipitation[is.na(fl_df$Place)])
+summary(fl_df$Precipitation[!is.na(fl_df$Place)])
 # The probability an avalanche will occur when it snows more than .21 mm is 48% as opposed to 19% chance of not having an avalanche when it snows more than .21mm
-length(aw_df$Precipitation[which(aw_df$Precipitation>=quantile(aw_df$Precipitation, 0.5, na.rm = TRUE))])/length(aw_df$Precipitation)
-length(no_a_df$Precipitation[which(no_a_df$Precipitation>=quantile(aw_df$Precipitation, 0.5, na.rm = TRUE))])/length(no_a_df$Precipitation)
+length(fl_df$Precipitation[which(fl_df$Precipitation[is.na(fl_df$Place)]>=quantile(fl_df$Precipitation[is.na(fl_df$Place)], 0.5, na.rm = TRUE))])/length(fl_df$Precipitation)
+length(fl_df$Precipitation[which(fl_df$Precipitation[!is.na(fl_df$Place)]>=quantile(fl_df$Precipitation[!is.na(fl_df$Place)], 0.5, na.rm = TRUE))])/length(fl_df$Precipitation)
 
-boxplot(no_a_df$Snowfall, aw_df$Snowfall)
-boxplot(no_a_df$Snow_Depth, aw_df$Snow_Depth)
-boxplot(no_a_df$Max_Temperature, aw_df$Max_Temperature)
-boxplot(no_a_df$Min_Temperature, aw_df$Min_Temperature)
-boxplot(no_a_df$Temperature_at_observation_time, aw_df$Temperature_at_observation_time)
-boxplot(no_a_df$Average_Wind_Speed, aw_df$Average_Wind_Speed)
-boxplot(no_a_df$Max_Wind_Speed, aw_df$Max_Wind_Speed)
+boxplot(fl_df$Snowfall, fl_df$Snowfall)
+boxplot(fl_df$Snow_Depth, fl_df$Snow_Depth)
+boxplot(fl_df$Max_Temperature, fl_df$Max_Temperature)
+boxplot(fl_df$Min_Temperature, fl_df$Min_Temperature)
+boxplot(fl_df$Temperature_at_observation_time, fl_df$Temperature_at_observation_time)
+boxplot(fl_df$Average_Wind_Speed, fl_df$Average_Wind_Speed)
+boxplot(fl_df$Max_Wind_Speed, fl_df$Max_Wind_Speed)
 
 #Quick look at things that might be associated
 #As min temp increases so does the liklihood of bodily injury
-summary(lm(aw_df_ik$BodilyHarmN~aw_df_ik$TMIN))
+summary(lm(fl_df_ik$BodilyHarmN~fl_df_ik$TMIN))
 #The ones below don't have much statistical significance
-summary(lm(aw_df_ik$BodilyHarmN~aw_df_ik$TMAX))
-summary(lm(aw_df_ik$BodilyHarmN~aw_df_ik$PRCP))
-summary(lm(aw_df_ik$BodilyHarmN~aw_df_ik$Elevation))
-summary(lm(aw_df_ik$BodilyHarmN~aw_df_ik$SNWD))
-summary(lm(aw_df_ik$BodilyHarmN~aw_df_ik$SNOW))
-summary(lm(aw_df_ik$BodilyHarmN~aw_df_ik$TMAX))
-summary(lm(aw_df_ik$BodilyHarmN~aw_df_ik$TOBS))
+summary(lm(fl_df_ik$BodilyHarmN~fl_df_ik$TMAX))
+summary(lm(fl_df_ik$BodilyHarmN~fl_df_ik$PRCP))
+summary(lm(fl_df_ik$BodilyHarmN~fl_df_ik$Elevation))
+summary(lm(fl_df_ik$BodilyHarmN~fl_df_ik$SNWD))
+summary(lm(fl_df_ik$BodilyHarmN~fl_df_ik$SNOW))
+summary(lm(fl_df_ik$BodilyHarmN~fl_df_ik$TMAX))
+summary(lm(fl_df_ik$BodilyHarmN~fl_df_ik$TOBS))
 
 #As min/max/observed temp increases going for a ride decreases, this data might be skewed because of the difference in the magnitudes of the numbers
-summary(lm(TypeOfRideN~TMIN, data = aw_df_ccb))
-summary(lm(TypeOfRideN~TMAX, data = aw_df_ccb))
-summary(lm(TypeOfRideN~TOBS, data = aw_df_ccb))
+summary(lm(TypeOfRideN~TMIN, data = fl_df_ccb))
+summary(lm(TypeOfRideN~TMAX, data = fl_df_ccb))
+summary(lm(TypeOfRideN~TOBS, data = fl_df_ccb))
 #As precipitation/Snowfall/Depth of snow increases going for a ride increases
-summary(lm(TypeOfRideN~PRCP, data = aw_df_ccb))
-summary(lm(TypeOfRideN~SNOW, data = aw_df_ccb))
-summary(lm(TypeOfRideN~SNWD, data = aw_df_ccb))
-summary(lm(TypeOfRideN~TMIN, data = aw_df_ccb))
-summary(lm(TypeOfRideN~TMIN, data = aw_df_ccb))
-summary(lm(TypeOfRideN~TMAX, data = aw_df_ccb))
-summary(lm(TypeOfRideN~TOBS, data = aw_df_ccb))
+summary(lm(TypeOfRideN~PRCP, data = fl_df_ccb))
+summary(lm(TypeOfRideN~SNOW, data = fl_df_ccb))
+summary(lm(TypeOfRideN~SNWD, data = fl_df_ccb))
+summary(lm(TypeOfRideN~TMIN, data = fl_df_ccb))
+summary(lm(TypeOfRideN~TMIN, data = fl_df_ccb))
+summary(lm(TypeOfRideN~TMAX, data = fl_df_ccb))
+summary(lm(TypeOfRideN~TOBS, data = fl_df_ccb))
 #As precipitation/Snowfall/Depth of snow increases going for a ride increases
-summary(lm(TypeOfRideN~PRCP, data = aw_df_ccb))
-summary(lm(TypeOfRideN~SNOW, data = aw_df_ccb))
+summary(lm(TypeOfRideN~PRCP, data = fl_df_ccb))
+summary(lm(TypeOfRideN~SNOW, data = fl_df_ccb))
 #The association below has an ok residual error compared to the others
-summary(lm(TypeOfRideN~SNWD, data = aw_df_ccb))
+summary(lm(TypeOfRideN~SNWD, data = fl_df_ccb))
 
 #As width of the avalanche increase, so does depth
 summary(lm(Depth~Width, data = avalInfo))
@@ -58,19 +58,19 @@ summary(lm(Depth~Width, data = avalInfo))
 summary(lm(Vertical~Elevation, data = avalInfo))
 
 #As Precipitation increases so does depth/width/vertical
-summary(lm(Depth~PRCP, data = aw_df))
-summary(lm(Width~PRCP, data = aw_df)) #With good significance levels and rejection of null
-summary(lm(Vertical~PRCP, data = aw_df)) #With good significance levels and rejection of null
+summary(lm(Depth~PRCP, data = fl_df))
+summary(lm(Width~PRCP, data = fl_df)) #With good significance levels and rejection of null
+summary(lm(Vertical~PRCP, data = fl_df)) #With good significance levels and rejection of null
 #As Snow depth increases so does depth/width/vertical/elevation
-summary(lm(Depth~SNWd, data = aw_df))
-summary(lm(Width~SNWD, data = aw_df)) #With good significance levels and rejection of null
-summary(lm(Vertical~SNWD, data = aw_df)) #With good significance levels and rejection of null
-summary(lm(Elevation~SNWD, data = aw_df)) #With good significance levels and rejection of null
+summary(lm(Depth~SNWd, data = fl_df))
+summary(lm(Width~SNWD, data = fl_df)) #With good significance levels and rejection of null
+summary(lm(Vertical~SNWD, data = fl_df)) #With good significance levels and rejection of null
+summary(lm(Elevation~SNWD, data = fl_df)) #With good significance levels and rejection of null
 #As Snowfall increases so does depth/width/vertical/elevation
-summary(lm(Depth~SNOW, data = aw_df)) 
-summary(lm(Width~SNOW, data = aw_df)) #With good significance levels and rejection of null
-summary(lm(Vertical~SNOW, data = aw_df)) #Can't reject null 
-summary(lm(Elevation~SNOW, data = aw_df)) #Can't reject null 
+summary(lm(Depth~SNOW, data = fl_df)) 
+summary(lm(Width~SNOW, data = fl_df)) #With good significance levels and rejection of null
+summary(lm(Vertical~SNOW, data = fl_df)) #Can't reject null 
+summary(lm(Elevation~SNOW, data = fl_df)) #Can't reject null 
 #As min temp increases so does depth/width/vertical/elevation
 
 
@@ -79,66 +79,66 @@ summary(lm(Elevation~SNOW, data = aw_df)) #Can't reject null
 library(ggplot2)
 #Plots
 #Plot Date
-qplot(data = aw_df, x=Date, bins=296)
+qplot(data = fl_df, x=Date, bins=296)
 #Plot Region
-qplot(data = aw_df, x=Region)
+qplot(data = fl_df, x=Region)
 #Plot Place
-qplot(data = aw_df, x=Place)
+qplot(data = fl_df, x=Place)
 #Plot Trigger
-qplot(data = aw_df, x=Trigger)
+qplot(data = fl_df, x=Trigger)
 #qplot(data = avalInfo, x=Trigger)
 #Plot Weak Layer
-qplot(data = aw_df, x=Weak.Layer)
+qplot(data = fl_df, x=Weak.Layer)
 #qplot(data = avalInfo, x=Weak.Layer)
 #Plot Depth
-ggplot(data = aw_df, aes(x=Depth))+
+ggplot(data = fl_df, aes(x=Depth))+
   geom_histogram(breaks=seq(0,120,1)) #+  xlim(c(0,60))
 #Plot Width
-ggplot(data = aw_df, aes(x=Width))+
+ggplot(data = fl_df, aes(x=Width))+
   geom_histogram(breaks=seq(5,1000,10)) + xlim(c(0,500))
 #Plot Vertical
-ggplot(data = aw_df, aes(x=Vertical))+
+ggplot(data = fl_df, aes(x=Vertical))+
   geom_histogram(breaks=seq(10,3500,10)) + xlim(c(10,2500))
 #Plot Aspect
-qplot(data = aw_df, x=Aspect)
+qplot(data = fl_df, x=Aspect)
 #Plot of Elevation
-ggplot(data = aw_df, aes(x=Elevation)) +
+ggplot(data = fl_df, aes(x=Elevation)) +
   geom_histogram( breaks=seq(7700,11200, 100)) +
   xlim(c(7700,11200))
 #Plot Latitude
-ggplot(data = aw_df, aes(x=Coordinates$Latitude))+
+ggplot(data = fl_df, aes(x=Coordinates$Latitude))+
   geom_histogram(breaks=seq(40.38,42.8,0.01)) +
   xlim(c(40.38,42.08))
 #Plot Longitude
-ggplot(data = aw_df, aes(x=Coordinates$Longitude))+
+ggplot(data = fl_df, aes(x=Coordinates$Longitude))+
   geom_histogram(breaks=seq(-112.25,-111.0,0.005))+
   xlim(c(-112.25,-111.0))
 #Plot how many were caught/carried/Buried
-qplot(data = aw_df_ccb, x=TypeOfRide)
+qplot(data = fl_df_ccb, x=TypeOfRide)
 #Plot how many were injured or killed
-qplot(data=aw_df_ik, x=BodilyHarm)
+qplot(data=fl_df_ik, x=BodilyHarm)
 #Plot Precipitation
-ggplot(data=aw_df, aes(x=Precipitation))+
+ggplot(data=fl_df, aes(x=Precipitation))+
   geom_histogram(breaks=seq(0, 3, 0.05)) #+ xlim(c(0.0001,3))
 #Plot Snow depth
-ggplot(aw_df, aes(x=Snow_Depth))+
+ggplot(fl_df, aes(x=Snow_Depth))+
   geom_histogram(breaks=seq(0,168,1))
 #Plot Snowfall
-ggplot(aw_df, aes(x=Snowfall)) +
+ggplot(fl_df, aes(x=Snowfall)) +
   geom_histogram(breaks=seq(0,32,0.2)) #+xlim(.01,22)
 #Plot max temperature
-ggplot(aw_df, aes(x=Max_Temperature))+
+ggplot(fl_df, aes(x=Max_Temperature))+
   geom_histogram(breaks=seq(1,61,1))
 #Plot minimum temperature
-ggplot(aw_df, aes(x=Min_Temperature))+
+ggplot(fl_df, aes(x=Min_Temperature))+
   geom_histogram(breaks=seq(-13,40,1))
 #Plot temperature at time of observation
-ggplot(aw_df, aes(x=Temperature_at_observation_time))+
+ggplot(fl_df, aes(x=Temperature_at_observation_time))+
   geom_histogram()
 # Plot average wind speed
-ggplot(aw_df, aes(x=Average_Wind_Speed))+
+ggplot(fl_df, aes(x=Average_Wind_Speed))+
   geom_histogram(breaks=seq(0, 6, .1))
 # Plot Highest wind speed
-ggplot(aw_df, aes(x=Max_Wind_Speed))+
+ggplot(fl_df, aes(x=Max_Wind_Speed))+
   geom_histogram(breaks=seq(0,31.6,.1))
 
