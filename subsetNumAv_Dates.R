01634a39ca1547d2f1ec5e9af996bfa0602c16eb
justinlboyer <justinlboyer@gmail.com>
2016-06-06 15:51:15 -0600

    Removed outlier dates Updated names

--- subsetNumAv_Dates.R
+++ subsetNumAv_Dates.R
@@ -13,6 +13,11 @@ temp <- ddply(avalInfo,.(Date),nrow)
 colnames(temp)[2] <- "NumberOfAvalanches"
 #Check that all the dates got encoded
 length(avalInfo$Date)==sum(temp$NumberOfAvalanches)
+#Now remove the dates deemed outliers (Any dates pre 2004)
+temp <- temp[grep("200[4-9]-|201[0-6]",temp$Date),]
+#Remove any NAs in Date
+temp <- temp[!is.na(temp$Date),]
+
 
 #Create data frame with single dates
 numav <- ddply(numav, .(Date), summarise, Depth.mean=mean(Depth),Width.mean=mean(Width),Vertical.mean=mean(Vertical),Elevation.mean=mean(Elevation),Lat.mean=mean(Latitude),Long.mean=mean(Longitude),Preci.mean=mean(Precipitation),SnowDepth.mean=mean(Snow_Depth),Snowfall.mean=mean(Snowfall),Max_Temperature.mean=mean(Max_Temperature),Min_Temperature.mean=mean(Min_Temperature),TempObs.mean=mean(Temperature_at_observation_time), AvgWindSpeed.mean=mean(Average_Wind_Speed),MaxWindSpeed.mean=mean(Max_Wind_Speed))
@@ -20,14 +25,14 @@ numav <- merge(numav, temp, by.x = 'Date', by.y = 'Date', all.x = TRUE, all.y = 
 #Replace NA with 0 in number of avalanches column
 numav$NumberOfAvalanches[is.na(numav$NumberOfAvalanches)] <- 0
 #Set values with NA and number of avalanches=0 to 0
-numav$Depth[numav$NumberOfAvalanches==0] <- 0
-numav$Width[numav$NumberOfAvalanches==0] <- 0
-numav$Vertical[numav$NumberOfAvalanches==0] <- 0
-numav$Elevation[numav$NumberOfAvalanches==0] <- 0
-numav$Latitude[numav$NumberOfAvalanches==0] <- 0
-numav$Longitude[numav$NumberOfAvalanches==0] <- 0
+numav$Depth.mean[numav$NumberOfAvalanches==0] <- 0
+numav$Width.mean[numav$NumberOfAvalanches==0] <- 0
+numav$Vertical.mean[numav$NumberOfAvalanches==0] <- 0
+numav$Elevation.mean[numav$NumberOfAvalanches==0] <- 0
+numav$Lat.mean[numav$NumberOfAvalanches==0] <- 0
+numav$Long.mean[numav$NumberOfAvalanches==0] <- 0
 
-#Check that there are enough avalanches (is this still necessary?)
+#Check that there are enough avalanches (is this still necessary)
 store =0
 for (i in 1:100){
   store = store + sum(numav$NumberOfAvalanches[numav$NumberOfAvalanches==i])
