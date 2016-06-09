dd14915432e2832d3205d77fd6e16e7b96cc6941
justinlboyer <justinlboyer@gmail.com>
2016-06-09 14:56:19 -0600

    Removed the values after being used and cleaned formatting

--- subsetNumAv_Dates.R
+++ subsetNumAv_Dates.R
@@ -22,6 +22,8 @@ temp <- temp[!is.na(temp$Date),]
 #Create data frame with single dates
 numav <- ddply(numav, .(Date), summarise, Depth.mean=mean(Depth),Width.mean=mean(Width),Vertical.mean=mean(Vertical),Elevation.mean=mean(Elevation),Lat.mean=mean(Latitude),Long.mean=mean(Longitude),Preci.mean=mean(Precipitation),SnowDepth.mean=mean(Snow_Depth),Snowfall.mean=mean(Snowfall),Max_Temperature.mean=mean(Max_Temperature),Min_Temperature.mean=mean(Min_Temperature),TempObs.mean=mean(Temperature_at_observation_time), AvgWindSpeed.mean=mean(Average_Wind_Speed),MaxWindSpeed.mean=mean(Max_Wind_Speed))
 numav <- merge(numav, temp, by.x = 'Date', by.y = 'Date', all.x = TRUE, all.y = TRUE)
+#Remove the temp df
+rm(temp)
 #Replace NA with 0 in number of avalanches column
 numav$NumberOfAvalanches[is.na(numav$NumberOfAvalanches)] <- 0
 #Set values with NA and number of avalanches=0 to 0
@@ -32,10 +34,8 @@ numav$Elevation.mean[numav$NumberOfAvalanches==0] <- 0
 numav$Lat.mean[numav$NumberOfAvalanches==0] <- 0
 numav$Long.mean[numav$NumberOfAvalanches==0] <- 0
 
-#Check that there are enough avalanches (is this still necessary)
-store =0
-for (i in 1:100){
-  store = store + sum(numav$NumberOfAvalanches[numav$NumberOfAvalanches==i])
-}
-print(store>=length(avalInfo[avalInfo$Region=='Sal',]))
+
+
+#Remove values that are no longer needed: i, store
+rm(i, store)
 
