53a8dfdfe57fbaf9b0d76ac49f948e9638be4e2c
Justin Boyer <justinlboyer@gmail.com>
2016-09-29 17:05:55 -0600

    Updated script to make aspect a usable variable, had to replace all NA in aspect with a weighted random selection of values

--- subsetNumAv_Dates.R
+++ subsetNumAv_Dates.R
@@ -19,10 +19,12 @@ temp <- temp[grep("200[4-9]-|201[0-6]",temp$Date),]
 temp <- temp[!is.na(temp$Date),]
 
 #Remove NAs in Aspect
-numav <-numav[!is.na(numav$Aspect),]
+#numav <-numav[!is.na(numav$Aspect),]
 
-#Create data frame with single dates
-numav <- ddply(numav, .(Date), summarise, Depth.mean=mean(Depth),Width.mean=mean(Width),Vertical.mean=mean(Vertical),Elevation.mean=mean(Elevation),Lat.mean=mean(Latitude),Long.mean=mean(Longitude),Preci.mean=mean(Precipitation),SnowDepth.mean=mean(Snow_Depth),Snowfall.mean=mean(Snowfall),Max_Temperature.mean=mean(Max_Temperature),Min_Temperature.mean=mean(Min_Temperature),TempObs.mean=mean(Temperature_at_observation_time), AvgWindSpeed.mean=mean(Average_Wind_Speed),MaxWindSpeed.mean=mean(Max_Wind_Speed),Aspect.count = Aspect[2]) #ifelse(length(names(table(Aspect)[which(table(numav$Aspect)==max(table(Aspect)))]))>1, "Northeast",names(table(Aspect)[which(table(numav$Aspect)==max(table(Aspect)))]))
+
+
+#Create data frame with single dates (updated to include aspect)
+numav <- ddply(numav, .(Date), summarise, Depth.mean=mean(Depth),Width.mean=mean(Width),Vertical.mean=mean(Vertical),Elevation.mean=mean(Elevation),Lat.mean=mean(Latitude),Long.mean=mean(Longitude),Preci.mean=mean(Precipitation),SnowDepth.mean=mean(Snow_Depth),Snowfall.mean=mean(Snowfall),Max_Temperature.mean=mean(Max_Temperature),Min_Temperature.mean=mean(Min_Temperature),TempObs.mean=mean(Temperature_at_observation_time), AvgWindSpeed.mean=mean(Average_Wind_Speed),MaxWindSpeed.mean=mean(Max_Wind_Speed),Aspect.count = sample(Aspect, size = 1)) #ifelse(length(names(table(Aspect)[which(table(numav$Aspect)==max(table(Aspect)))]))>1, "Northeast",names(table(Aspect)[which(table(numav$Aspect)==max(table(Aspect)))]))
 numav <- merge(numav, temp, by.x = 'Date', by.y = 'Date', all.x = TRUE, all.y = TRUE)
 #Remove the temp df
 rm(temp)
@@ -36,9 +38,15 @@ numav$Elevation.mean[numav$NumberOfAvalanches==0] <- 0
 numav$Lat.mean[numav$NumberOfAvalanches==0] <- 0
 numav$Long.mean[numav$NumberOfAvalanches==0] <- 0
 
+#Set NA's in Aspect to random aspect, so that aspect is usable
+Ns <- length(numav$Aspect.count[is.na(numav$Aspect.count)])
+numav$Aspect.count[is.na(numav$Aspect.count)] <- sample(levels(numav$Aspect.count)[2:9],Ns, replace = TRUE)
+Bls <- length(numav$Aspect.count[numav$Aspect.count==""])
+numav$Aspect.count[numav$Aspect.count==""] <- sample(levels(numav$Aspect.count)[2:9],Bls, replace = TRUE)
+
 
 
 
 #Remove values that are no longer needed: i, store
-rm(i, store)
+rm(Bls, Ns)
 
